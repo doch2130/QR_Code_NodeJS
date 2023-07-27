eafa751d36e223cf62bf5f23fc5a21cadcfa112e
HyoHyun_Home <doch2130@gmail.com>
2023-07-27 23:59:19 +0900

    feat: 2023-07-27 2359 qrcode test

---
+++ test.js
@@ -0,0 +1,71 @@
+const qr = require('qrcode');
+const fs = require('fs');
+const Jimp = require('jimp');
+
+const dataToEncode = 'Hello, World2222!'; // QR 코드로 인코딩할 데이터
+
+// QR 코드 옵션 설정
+const options = {
+  // 기본적인 옵션
+  errorCorrectionLevel: 'H', // 오류 정정 레벨 (L, M, Q, H)
+  type: 'image/png', // 이미지 타입 (jpeg, png 등)
+  margin: 2, // QR 코드 주변 여백
+  scale: 8, // QR 코드 크기
+
+  // 커스텀 옵션
+  color: {
+    dark: '#000000', // QR 코드의 블록(암호화된 부분) 색상
+    light: '#ffffff', // QR 코드의 빈 공간(암호화되지 않은 부분) 색상
+  },
+};
+
+// QR 코드 생성
+qr.toFile('custom_qrcode2.png', dataToEncode, options, (err) => {
+  if (err) {
+    console.error('Error generating QR code:', err);
+  } else {
+    console.log('QR code successfully generated!');
+    applyDesignToQRCode();
+  }
+});
+
+async function applyDesignToQRCode() {
+  // 이 함수에서는 생성된 QR 코드 이미지에 디자인을 적용하는 작업을 수행합니다.
+  // 예를 들어, 다른 이미지를 로드하여 코드 부분을 감싸는 등의 디자인 변경이 가능합니다.
+  // 이 부분은 간략한 예제이므로, 실제로 어떤 디자인을 적용할지는 상황에 맞게 결정해야 합니다.
+  // 다양한 이미지 편집 도구 또는 그래픽 디자인 도구를 사용하여 작업할 수 있습니다.
+
+  // const backgroundImageFile = 'testtest.png'; // 디자인할 이미지 파일 (예시: background.png)
+  const backgroundImageFile = 'testtest2.png'; // 디자인할 이미지 파일 (예시: background.png)
+
+  try {
+    // 이미지 파일 로드
+    const qrCodeImage = await Jimp.read('custom_qrcode2.png');
+    const backgroundImage = await Jimp.read(backgroundImageFile);
+
+    // QR 코드 이미지의 크기에 맞게 배경 이미지를 리사이즈
+    backgroundImage.resize(
+      qrCodeImage.bitmap.width / 2,
+      qrCodeImage.bitmap.height / 2
+    );
+
+    // 배경 이미지가 보이지만, 카메라가 QR Code로 인식을 못함
+    // QR 코드 이미지와 배경 이미지를 합성하여 디자인 적용
+    // qrCodeImage.composite(backgroundImage, 0, 0);
+    qrCodeImage.composite(
+      backgroundImage,
+      qrCodeImage.bitmap.width / 4,
+      qrCodeImage.bitmap.height / 4
+    );
+
+    // 배경 이미지가 qr code 뒤로가서 전혀 안보임
+    // qrCodeImage.composite(backgroundImage, 0, 0, {
+    //   mode: Jimp.BLEND_DESTINATION_OVER, // 배경 이미지가 뒤로 오도록 합성 모드 설정
+    // });
+
+    // 합성된 이미지를 파일로 저장
+    await qrCodeImage.writeAsync('qrcode_with_design.png');
+  } catch (error) {
+    console.error('Error applying design to QR code:', error);
+  }
+}
